c7c619281e083208c14672ec85bc27333a7d939c
zpqqq10 <2601087520@qq.com>
2021-06-03 12:40:32 +0800

    some update

--- catalog.py
+++ catalog.py
@@ -2,6 +2,23 @@ import os
 import struct
 import sys
 
+class Table():
+    def __init__(self, table_name, primary_key, number_attributes):
+        self.table_name = table_name
+        self.primary_key = primary_key
+        self.attributes = []
+        self.number_attributes = number_attributes
+
+
+# type = int, char(n) and 1<=n<=255, float
+# length is n
+class Attribute():
+    def __init__(self, name, type='20s', length=20, uniqueness=False):
+        self.name = name
+        self.uniqueness = uniqueness
+        self.type = type
+        self.length = length
+
 class catalog_manager:
     def __init__(self):
         self.tables = {}  # dictionary for all tables
@@ -9,32 +26,32 @@ class catalog_manager:
         # build tables
         os.chdir(sys.path[0])
         file = open('./catalog/table_catalog.dat', 'rb')
-        table_num = struct.unpack('i', file.read(4))
+        table_num, = struct.unpack('=i', file.read(4))   # the number of all the tables
         for i in range(table_num):
-            len_tbl, len_pky, len_attr = struct.unpack('iii', file.read(3*4))
-            table_name, primary = struct.unpack(str(len_tbl)+'s'+str(len_pky)+'s', file.read(len_tbl+len_pky))
-            self.tables[table_name] = Table(table_name, primary, len_attr)  # read a table
-            for i in range(len_attr): 
-                len_name, = struct.unpack('i', file.read(4))
-                attr_name, = struct.unpack(str(len_name)+'s', file.read(len_name))
-                uniqueness, = struct.unpack('?', file.read(1))
-                type, = struct.unpack('i', file.read(4))
+            len_tbl, len_pky, num_attr = struct.unpack('=iii', file.read(3*4))
+            table_name, primary = struct.unpack('='+str(len_tbl)+'s'+str(len_pky)+'s', file.read(len_tbl+len_pky))
+            self.tables[table_name] = Table(table_name, primary, num_attr)  # read a table
+            for i in range(num_attr): 
+                len_name, = struct.unpack('=i', file.read(4))
+                attr_name, = struct.unpack('='+str(len_name)+'s', file.read(len_name))
+                uniqueness, = struct.unpack('=?', file.read(1))
+                type, = struct.unpack('=i', file.read(4))
                 if type == 1: 
-                    self.tables[attr_name] = Attribute(uniqueness, 'int', 0)
+                    self.tables[table_name].attributes.append(Attribute(attr_name, uniqueness, 'i', 0))
                 elif type == 2: 
-                    self.tables[attr_name] = Attribute(uniqueness, 'float', 0)
+                    self.tables[table_name].attributes.append(Attribute(attr_name, uniqueness, 'f', 0))
                 elif type == 3:
-                    length, = struct.unpack('i', file.read(4))
-                    self.tables[attr_name] = Attribute(uniqueness, 'char', length)
+                    length, = struct.unpack('=i', file.read(4))
+                    self.tables[table_name].attributes.append(Attribute(uniqueness, 's', length))
         file.close()
         # build indices
         file = open('./catalog/index_catalog.dat', 'rb')
-        index_num = struct.unpack('i', file.read(4))
+        index_num, = struct.unpack('=i', file.read(4))
         for i in range(index_num):
-            len_index, len_tbl, len_key = struct.unpack('iii', file.read(3*4))
-            index = struct.unpack(str(len_index)+'s', file.read(len_index))
-            tbl   = struct.unpack(str(len_tbl)+'s', file.read(len_tbl))
-            key   = struct.unpack(str(len_key)+'s', file.read(len_key))
+            len_index, len_tbl, len_key = struct.unpack('=iii', file.read(3*4))
+            index = struct.unpack('='+str(len_index)+'s', file.read(len_index))
+            tbl   = struct.unpack('='+str(len_tbl)+'s', file.read(len_tbl))
+            key   = struct.unpack('='+str(len_key)+'s', file.read(len_key))
             self.indices[index] = [tbl, key]
         file.close()
     
@@ -43,127 +60,116 @@ class catalog_manager:
         # save tables
         os.chdir(sys.path[0])
         file = open('./catalog/table_catalog.dat', 'wb+')
-        file.write(struct.pack('i', len(self.tables)))  # the number of tables
+        file.write(struct.pack('=i', len(self.tables)))  # the number of tables
         for table in self.tables: 
             len_tbl = len(table) # length of table name
             len_pky = len(self.tables[table].primary_key)# length of the name of the primary key
-            file.write(struct.pack('iii', len_tbl, len_pky, len(self.tables[table].attributes.keys())))
-            file.write(struct.pack(str(len_tbl)+'s'+str(len_pky)+'s', table.encode('utf-8'), self.tables[table].primary_key.encode('utf-8')))
-            for key in self.tables[table].attributes:
-                len_name = len(key)
-                file.write(struct.pack('i'+str(len_name)+'s', len_name, key.encode('utf-8')))   # name
-                file.write(struct.pack('?', self.tables[table].attributes[key].uniqueness))   # uniqueness
-                if self.tables[table].attributes[key].type == 'int':                    # type
-                    file.write(struct.pack('i', 1))
-                elif self.tables[table].attributes[key].type == 'float': 
-                    file.write(struct.pack('i', 2))
-                elif self.tables[table].attributes[key].type == 'char': 
-                    file.write(struct.pack('ii', 3, self.tables[table].attributes[key].length))
+            file.write(struct.pack('=iii', len_tbl, len_pky, len(self.tables[table].attributes)))
+            file.write(struct.pack('='+str(len_tbl)+'s'+str(len_pky)+'s', table.encode('utf-8'), self.tables[table].primary_key.encode('utf-8')))
+            print(3)
+            for attr in self.tables[table].attributes:
+                len_name = len(attr.name)
+                file.write(struct.pack('=i'+str(len_name)+'s', len_name, attr.name.encode('utf-8')))   # name
+                file.write(struct.pack('=?', attr.uniqueness))   # uniqueness
+                if attr.type == 'i':                    # type
+                    file.write(struct.pack('=i', 1))
+                elif attr.type == 'f': 
+                    file.write(struct.pack('=i', 2))
+                elif attr.type[:-1] == 's': 
+                    file.write(struct.pack('=ii', 3, attr.length))
         file.close()
         # save indices
         file = open('./catalog/index_catalog.dat', 'wb+')
-        file.write(struct.pack('i', len(self.indices)))  # the number of indices
+        file.write(struct.pack('=i', len(self.indices)))  # the number of indices
         for index in self.indices: 
             len_index = len(index)
             len_tbl = len(self.indices[index][0])
             len_key = len(self.indices[index][1])
-            file.write(struct.pack('iii', len_index, len_tbl, len_key))
-            file.write(struct.pack(str(len_index)+'s', index.encode('utf-8')))                  # name of index
-            file.write(struct.pack(str(len_tbl)+'s', self.indices[index][0].encode('utf-8')))   # name of table
-            file.write(struct.pack(str(len_key)+'s', self.indices[index][1].encode('utf-8')))   # name of key
+            file.write(struct.pack('=iii', len_index, len_tbl, len_key))
+            file.write(struct.pack('='+str(len_index)+'s', index.encode('utf-8')))                  # name of index
+            file.write(struct.pack('='+str(len_tbl)+'s', self.indices[index][0].encode('utf-8')))   # name of table
+            file.write(struct.pack('='+str(len_key)+'s', self.indices[index][1].encode('utf-8')))   # name of key
         file.close()
-
-class Table():
-    def __init__(self, table_name, primary_key, number_attributes):
-        self.table_name = table_name
-        self.primary_key = primary_key
-        self.attributes = {}
-        self.number_attributes = number_attributes
+    
+    def __del__(self): 
+        self.save()
+        print('del catalog_manager')
 
 
-# type = int, char(n) and 1<=n<=255, float
-# length is n
-class Attribute():
-    def __init__(self, uniqueness=False, type='char', length=20):
-        # self.attribute_name = name
-        self.uniqueness = uniqueness
-        self.type = type
-        self.length = length
+    # raise an exception if the table exists
+    def table_exists(self, name):
+        # for tbl_name in tables.keys():
+        #     if name == tbl_name:
+        #         raise Exception("Table '%s' exists" % name)
+        if name in self.tables.keys(): 
+            raise Exception("Table '%s' exists" % name)
 
 
-# raise an exception if the table exists
-def table_exists(name):
-    # for tbl_name in tables.keys():
-    #     if name == tbl_name:
-    #         raise Exception("Table '%s' exists" % name)
-    if name in catalog_manager.tables.keys(): 
-        raise Exception("Table '%s' exists" % name)
+    # raise an exception if the table does not exist
+    def table_not_exists(self, name):
+        if name not in self.tables.keys(): 
+            raise Exception("Table '%s' doesn't exist" % name)
 
 
-# raise an exception if the table does not exist
-def table_not_exists(name):
-    if name not in catalog_manager.tables.keys(): 
-        raise Exception("Table '%s' doesn't exist" % name)
+    # raise an exception if the index exists
+    def index_exists(self, name):
+        if name in self.indices.keys(): 
+            raise Exception("Index '%s' exists" % name)
 
 
-# raise an exception if the index exists
-def index_exists(name):
-    if name in catalog_manager.indices.keys(): 
-        raise Exception("Index '%s' exists" % name)
+    # raise an exception if the index does not exist
+    def index_not_exists(self, name):
+        if name not in self.indices.keys(): 
+            raise Exception("Index '%s' doesn't exist" % name)
 
 
-# raise an exception if the index does not exist
-def index_not_exists(name):
-    if name not in catalog_manager.indices.keys(): 
-        raise Exception("Index '%s' doesn't exist" % name)
+    # raise an exception if the key does not exist in the table
+    def key_not_exists(self, tbl_name, key):
+        for att in self.tables[tbl_name].attributes:
+            if key == att.name: 
+                return 
+        raise Exception("Key '%s' doesn't exist in table ''" % (key, tbl_name))
 
 
-# raise an exception if the key does not exist in the table
-def key_not_exists(tbl_name, key):
-    for att in catalog_manager.tables[tbl_name].attributes:
-        if key == att.name: 
-            return 
-    raise Exception("Key '%s' doesn't exist in table ''" % (key, tbl_name))
+    # raise an exception if the key is not unique
+    def key_not_unique(self, tbl_name, key):
+        if self.tables[tbl_name].attributes[key].uniqueness is False: 
+            raise Exception("The key '%s' is not unique" % key)
 
 
-# raise an exception if the key is not unique
-def key_not_unique(tbl_name, key):
-    if catalog_manager.tables[tbl_name].attributes[key].uniqueness is False: 
-        raise Exception("The key '%s' is not unique" % key)
+    # update the file & tables
+    # assume that the values have been processed by the interpreter
+    def create_table(self, tbl_name, primary_key, attrlist):
+        tmp = Table(tbl_name, primary_key, len(attrlist))
+        for attr in attrlist: 
+            tmp.attributes.append(Attribute(attr[0], attr[1], attr[2], attr[3]))
+        self.tables[tbl_name] = tmp
+        # file?
+        print("Successfully create table '%s'" % tbl_name)
 
 
-# update the file & tables
-# assume that the values have been processed by the interpreter
-def create_table(tbl_name, primary_key, attrlist):
-    global tables
-    tmp = Table(tbl_name, primary_key, len(attrlist))
-    for attr in attrlist: 
-        tmp.attributes[attr[0]] = Attribute(attr[1], attr[2], attr[3])
-    catalog_manager.tables[tbl_name] = tmp
-    # file?
-    print("Successfully create table '%s'" % tbl_name)
+    # update the file & tables
+    def drop_table(self, tbl_name):
+        self.tables.pop(tbl_name)
+        # file?
+        print("Successfully drop table '%s'" % tbl_name)
 
 
-# update the file & tables
-def drop_table(tbl_name):
-    global tables
-    catalog_manager.tables.pop(tbl_name)
-    # file?
-    print("Successfully drop table '%s'" % tbl_name)
+    # update the file & indices
+    def create_index(self, index_name, tbl_name, key):
+        self.key_not_exists(tbl_name, key)
+        self.key_not_unique(tbl_name, key)
+        self.indices[index_name] = [tbl_name, key]
+        # index.build_index()
 
 
-# update the file & indices
-def create_index(index_name, tbl_name, key):
-    global indices
-    key_not_exists(tbl_name, key)
-    key_not_unique(tbl_name, key)
-    catalog_manager.indices[index_name] = [tbl_name, key]
-    # index.build_index()
+    # update the file & indices
+    def drop_index(self, index_name):
+        self.indices.pop(index_name)
+        # the catalog file?
+        print("Successfully drop index '%s'" % index_name)
 
-
-# update the file & indices
-def drop_index(index_name):
-    global indices
-    catalog_manager.indices.pop(index_name)
-    # the catalog file?
-    print("Successfully drop index '%s'" % index_name)
+t = catalog_manager()
+t.create_table('xyz', 'sid', [['sid', '11s', 11, True], ['name', '3s', 3, False], ['sex', 'i', 20, False]])
+t.create_table('abc', 'sid', [['sid', '20s', 20, True], ['name', '3s', 3, False], ['sex', 'i', 20, False]])
+t.drop_table('xyz')
--- record.py
+++ record.py
@@ -2,18 +2,22 @@ import struct
 import os
 import sys
 from functools import reduce
+
+
 class record_manager:
     def __init__(self):
         print(1)
 
+    # attr here is teh format of struct
     def insert(self, tbl_name, attr, value):
         # temporarily use file open, lately will use buffer
-        f = open("./record/"+tbl_name, "rb+")
+        f = open("./record/"+tbl_name+".rec", "rb+")
         bytes = f.read(8)
         # buffer.read(tbl_name,0,0,8)
         print(bytes)
-        valid_bytes,free_bid, free_off, tail_flag, length = struct.unpack("=?hh?h", bytes)
-        print(valid_bytes,free_bid, free_off, length, tail_flag)
+        valid_bytes, free_bid, free_off, tail_flag, length = struct.unpack(
+            "=?hh?h", bytes)
+        print(valid_bytes, free_bid, free_off, length, tail_flag)
         if tail_flag == False:
             # wirte the record in free_bid:free_off
             f.seek(free_bid * 4096 + free_off * 8, 0)
@@ -23,8 +27,8 @@ class record_manager:
 
         length_ = sum([i[2] for i in attr]) + 1
         pad_num = (length << 3) - length_
-        bytes = struct.pack("=?",True)+reduce(lambda x, y: x+y, map(lambda x,
-                                             y: struct.pack(x[1], y), attr, value))
+        bytes = struct.pack("=?", True)+reduce(lambda x, y: x+y, map(lambda x,
+                                                                     y: struct.pack(x[1], y), attr, value))
         bytes += pad_num * struct.pack("=x")
         print(bytes)
         # wirte the record in free_bid:free_off
@@ -40,23 +44,27 @@ class record_manager:
                 next_free_bid = free_bid + 1
                 next_free_off = 0
             # print(next_free_bid,next_free_off,new_off)
-            bytes = struct.pack("=?hh?",False, next_free_bid, next_free_off, True)
+            bytes = struct.pack(
+                "=?hh?", False, next_free_bid, next_free_off, True)
         else:
             bytes = pre_free_list
         f.seek(0, 0)
         f.write(bytes)
+        f.close()
         # buffer.write(tbl_name,0,0,bytes,6)
 
     def delete_with_index(self, tbl_name, bid, off):
         # temporarily use file open, lately will use buffer
-        f = open("./record/"+tbl_name, "rb+")
+        f = open("./record/"+tbl_name+".rec", "rb+")
         free_list = f.read(6)                           # read the free_list
         f.seek(bid * 4096 + off * 8, 0)
         f.write(free_list)
         # buffer.write(tbl_name,bid,off,free_list,6)
-        bytes = struct.pack("=?hh?",False,bid,off,False)
-        f.seek(0,0)
+        bytes = struct.pack("=?hh?", False, bid, off, False)
+        f.seek(0, 0)
         f.write(bytes)
+        f.close()
+
     def create(self, tbl_name, attr):
         # the header of a record file, every record align to 8
         # 0             the bool the valid byte
@@ -68,25 +76,27 @@ class record_manager:
         length = sum([i[2] for i in attr]) + 1
         # length = sum(attr[i][2]) mod 8
         length = (length % 8 != 0) + (length >> 3)
-        content = struct.pack("=?hh?h", False,0, 1, True, length)
+        content = struct.pack("=?hh?h", False, 0, 1, True, length)
         print(content)
-        with open("./record/"+tbl_name, "wb") as f:
+        with open("./record/"+tbl_name+".rec", "wb") as f:
             f.write(content)
             f.close()
         return 0
-    def scan_all(self,attr):
+
+    def scan_all(self, attr):
         print(1)
 
+
 if __name__ == "__main__":
-    # test 
+    # test
     os.chdir(sys.path[0])
     t = record_manager()
-    t.create("abc",[("a","l",4),("b","l",4)])
+    t.create("abc", [("a", "l", 4), ("b", "l", 4)])
     t.insert("abc", [("a", "i", 4), ("b", "i", 4)], (4, 8))
     t.insert("abc", [("a", "i", 4), ("b", "i", 4)], (4, 8))
     t.insert("abc", [("a", "i", 4), ("b", "i", 4)], (5, 6))
     t.insert("abc", [("a", "i", 4), ("b", "i", 4)], (3, 7))
-    t.delete_with_index("abc",0,3)
-    t.delete_with_index("abc",0,1)
+    t.delete_with_index("abc", 0, 3)
+    t.delete_with_index("abc", 0, 1)
     # t.insert("abc", [("a", "i", 4), ("b", "i", 4)], (5, 6))
     # t.insert("abc", [("a", "i", 4), ("b", "i", 4)], (3, 7))
