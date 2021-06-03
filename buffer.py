06f17732485aca8a903a642d2480569975298fab
zpqqq10 <2601087520@qq.com>
2021-06-03 17:01:28 +0800

    a little buffer

--- buffer.py
+++ buffer.py
@@ -1,15 +1,28 @@
-def read_block(path, block_id): 
-    pass
+import os
+import sys
+block_size = 4096
 
-def write_blcok(path, block_id, content): 
-    pass
+class Buffer():
+    def __init__():
+        pass
+    
+    # relative path
+    def read_block(path, block_id): 
+        os.chdir(sys.path[0])
+        file = open(path, "rb")
+        file.seek(block_size*block_id, 0)
+        content = file.read(block_size)
+        return content
 
-def new_block(): 
-    # buffer or memory management
-    return bytearray(4096)
+    def write_blcok(path, block_id, content): 
+        pass
 
-def read_content(path, position, n):	# read the content of length n
-    pass
+    def new_block(): 
+        # buffer or memory management
+        return bytearray(4096)
 
-def commit(path): 	# save the update of the file?
-    pass
+    def read_content(path, position, n):	# read the content of length n
+        pass
+
+    def commit(path): 	# save the update of the file?
+        pass
