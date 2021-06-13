2775aab3c8d7bf32bbf2a19e696752c58717ea9b
N7Utb <765228482@qq.com>
2021-06-13 16:14:35 +0800

    na~ api

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 from catalog import catalog_manager
 from interpreter import interpreter,set_catalog
-
+from buffer import bufferManager
+from record import record_manager
+from index import index_manager
 banner1 = \
 '''
                                         __     
@@ -22,7 +24,10 @@ banner2 = \
 if __name__ == "__main__":
     print(banner2)
     catalog = catalog_manager()
-    set_catalog(catalog)
-    while True:
-        data = input("sql>")
-        interpreter(data)
+    buffer = bufferManager()
+    record = record_manager(buffer)
+    index = index_manager(buffer)
+    # set_catalog(catalog)
+    # while True:
+    #     data = input("sql>")
+    #     interpreter(data)
