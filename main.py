6d5924ad52ca1aacfe2067bd6ee283cf5cd93833
zpqqq10 <2601087520@qq.com>
2021-06-17 22:11:18 +0800

    modification

--- main.py
+++ main.py
@@ -26,10 +26,23 @@ banner = \
 
 if __name__ == "__main__":
     print(banner)
-    api = API()
     catalog = catalog_manager()
+    buffer = bufferManager()
+    record = record_manager(buffer)
+    index = index_manager(buffer)
+    api = API(catalog, buffer, record, index)
     set_catalog(catalog)
     set_api(api)
+    # support SHOW in the future
+    print('tables:', end='')
+    for tbl_name in catalog.tables.keys(): 
+        print(' '+tbl_name, end='')
+    print()
+    print('indice:', end='')
+    for idx_name in catalog.indices.keys(): 
+        print(' '+idx_name, end='')
+    print()
+    
     while True:
         try:
             data = input("sql>")
